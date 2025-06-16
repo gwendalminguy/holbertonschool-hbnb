21ef99fc8d6a2d484204371988939740628c07e5
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-16 09:42:39 +0200

    Added user methods for HBnBFacade

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -9,7 +9,15 @@ class HBnBFacade:
         self.amenity_repo = InMemoryRepository()
 
     def create_user(self, user_data):
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
 
     def get_place(self, place_id):
         pass
