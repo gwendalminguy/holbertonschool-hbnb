e28b0f64b39660ae1c4bbaa916de5075d1f5b04f
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-16 11:53:09 +0200

    Corrected users_ns name

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -6,5 +6,5 @@ def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
-    api.add_namespace(isers_ns, path='/api/v1/users')
+    api.add_namespace(users_ns, path='/api/v1/users')
     return app
