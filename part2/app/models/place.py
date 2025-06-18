28b382ea703a1013e5b11d7d8b41a5ec6bf79e58
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-18 17:18:19 +0200

    Removed owner from Place parameters

--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -2,7 +2,7 @@ from app.models.model import BaseModel
 
 
 class Place(BaseModel):
-    def __init__(self, title, description, price, latitude, longitude, owner_id, owner, rooms, capacity=0, surface=0, amenities=[]):
+    def __init__(self, title, description, price, latitude, longitude, owner_id, rooms, capacity=0, surface=0, amenities=[]):
         super().__init__()
         if len(title) <= 100:
             self.__title = title
