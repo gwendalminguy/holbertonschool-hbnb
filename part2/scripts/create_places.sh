b53e8c753f8e9dad475983d8aedd4286fe0480d4
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-18 09:07:01 +0200

    Added dynamic user ID retrieval to create a place

--- part2/scripts/create_places.sh
+++ part2/scripts/create_places.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo -e "\n> Create New User:"
-curl -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/json" -d '{"first_name": "John", "last_name": "Doe", "email": "john.doe@example.com"}'
+RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/json" -d '{"first_name": "John", "last_name": "Doe", "email": "john.doe@example.com"}')
+export OWNER="$(echo "$RESPONSE" | jq -r '.id')"
+echo "User ID: $OWNER"
 
-#Add dynamic owner_id retrieval
 echo -e "\n> Create New Place:"
-curl -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -d '{"title": "Californian Apartment", "description": "Beautiful place with a view on the beach.", "price": 550.0, "latitude": 65.5, "longitude": -172.5, "owner_id": "d0200297-0c1b-40b5-9f0d-05e11f0c9f1d", "rooms": 6, "capacity": 8, "surface": 85.5}'
+curl -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -d '{"title": "Californian Apartment", "description": "Beautiful place with a view on the beach.", "price": 550.0, "latitude": 65.5, "longitude": -172.5, "owner_id": "'"$OWNER"'", "rooms": 6, "capacity": 8, "surface": 85.5}'
