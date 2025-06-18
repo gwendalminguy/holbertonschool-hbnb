fe2e6035b43d052c27ea2f07cf5da369faa5cd92
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-18 16:50:32 +0200

    Updated bash script

--- part2/scripts/create_places.sh
+++ part2/scripts/create_places.sh
@@ -7,8 +7,4 @@ echo "User ID: $OWNER"
 
 echo -e "\n> Create New Places:"
 curl -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -d '{"title": "Californian Apartment", "description": "Beautiful place with a view on the beach.", "price": 550.0, "latitude": 65.5, "longitude": -172.5, "owner_id": "'"$OWNER"'", "rooms": 6, "capacity": 8, "surface": 85.5}'
-RESPONSE=$(curl -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -d '{"title": "Nice French Apartment", "description": "Cozy apartment in the center of Toulouse", "price": 225.0, "latitude": 85, "longitude": 125, "owner_id": "'"$OWNER"'", "rooms": 3, "capacity": 4, "surface": 55.5}')
-export PLACE="$(echo "$RESPONSE" | jq -r '.id')"
-
-echo -e "\n> Get Place:"
-curl -X GET http://localhost:5000/api/v1/places/$PLACE
+curl -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -d '{"title": "Nice French Apartment", "description": "Cozy apartment in the center of Toulouse", "price": 225.0, "latitude": 85, "longitude": 125, "owner_id": "'"$OWNER"'", "rooms": 3, "capacity": 4, "surface": 55.5}'
---
+++ part2/scripts/create_places_with_amenities.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "\n> Create New User:"
+RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/json" -d '{"first_name": "John", "last_name": "Doe", "email": "john.doe@example.com"}')
+export OWNER="$(echo "$RESPONSE" | jq -r '.id')"
+echo "User ID: $OWNER"
+
+echo -e "\n> Create New Amenities:"
+RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/amenities/ -H "Content-Type: application/json" -d '{"name": "Parking"}')
+export AMENITY_1="$(echo "$RESPONSE" | jq -r '.id')"
+echo "Amenity ID: $AMENITY_1"
+RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/amenities/ -H "Content-Type: application/json" -d '{"name": "Wi-Fi"}')
+export AMENITY_2="$(echo "$RESPONSE" | jq -r '.id')"
+echo "Amenity ID: $AMENITY_2"
+
+echo -e "\n> Create New Place:"
+RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -d '{"title": "Nice French Apartment", "description": "Cozy apartment in the center of Toulouse", "price": 225.0, "latitude": 85, "longitude": 125, "owner_id": "'"$OWNER"'", "rooms": 3, "capacity": 4, "surface": 55.5, "amenities": ["'"$AMENITY_1"'", "'"$AMENITY_2"'"]}')
+export PLACE="$(echo "$RESPONSE" | jq -r '.id')"
+echo "Place ID: $PLACE"
+
+echo -e "\n> Get Place:"
+curl -X GET http://localhost:5000/api/v1/places/$PLACE
