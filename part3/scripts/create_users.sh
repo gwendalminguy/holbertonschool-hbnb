b66a446579d3d28b3c867799588c644dc0d8de3c
Mornac <imornac@gmail.com>
2025-06-30 10:54:13 +0200

    update scripts create_users.sh to use password

--- part3/scripts/create_users.sh
+++ part3/scripts/create_users.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo -e "\n> Create New Users:"
-curl -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/json" -d '{"first_name": "Hugo", "last_name": "Chilemne", "email": "hugo.chilemne@example.com"}'
-curl -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/json" -d '{"first_name": "Fabien", "last_name": "Chavonet", "email": "fabien.chavonet@example.com"}'
+curl -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/json" -d '{"first_name": "Hugo", "last_name": "Chilemne", "email": "hugo.chilemne@example.com", "password": "abcd1234"}'
+curl -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/json" -d '{"first_name": "Fabien", "last_name": "Chavonet", "email": "fabien.chavonet@example.com", "password": "1234dcba"}'
