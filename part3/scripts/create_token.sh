25b36fb7fdaaefe375e76ff3ad94528792759097
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-30 14:12:26 +0200

    Updated bash script

--- part3/scripts/create_token.sh
+++ part3/scripts/create_token.sh
@@ -9,7 +9,12 @@ curl -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/j
 }'
 
 echo -e "\n> Create Token:"
-curl -X POST http://localhost:5000/api/v1/auth/login -H "Content-Type: application/json" -d '{
+RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/auth/login -H "Content-Type: application/json" -d '{
 	"email": "jane.doe@example.com",
 	"password": "abcd1234"
-}'
+}')
+export JWT="$(echo "$RESPONSE" | jq -r '.access_token')"
+echo "Access Token: $JWT"
+
+echo -e "\n> Access Protected Route:"
+curl -X GET http://127.0.0.1:5000/api/v1/auth/protected -H "Authorization: Bearer $JWT"
