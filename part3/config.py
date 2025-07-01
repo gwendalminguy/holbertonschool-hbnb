2657552baeb85bbd63d9bd2852d86d42b20a2976
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-01 16:26:36 +0200

    Added missing '/' in sqlite URI

--- part3/config.py
+++ part3/config.py
@@ -9,7 +9,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite://development.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
