5bc2f73f65c4f4ef873dbbfcf1271f03f6b689f0
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-01 09:18:14 +0200

    Updated bash script

--- part3/scripts/update_user.sh
+++ part3/scripts/update_user.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 echo -e "\n> Create New User:"
-RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/json" -d '{"first_name": "Hugo", "last_name": "Chilemme", "email": "hugo.chilemme@example.com"}')
+RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/json" -d '{"first_name": "Hugo", "last_name": "Chilemme", "email": "hugo.chilemme@example.com", "password": "ABCD1234"}')
 export USER="$(echo "$RESPONSE" | jq -r '.id')"
 echo "User ID: $USER"
+
+echo -e "\n> Create Token:"
+RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/auth/login -H "Content-Type: application/json" -d '{
+	"email": "hugo.chilemme@example.com",
+	"password": "ABCD1234"
+}')
+export JWT="$(echo "$RESPONSE" | jq -r '.access_token')"
+echo "Access Token: $JWT"
 
 echo -e "\n> Get User Details:"
 curl -X GET http://localhost:5000/api/v1/users/$USER
 
 echo -e "\n> Update User:"
-curl -X PUT http://localhost:5000/api/v1/users/$USER -H "Content-Type: application/json" -d '{"first_name": "Fabien", "last_name": "Chavonet", "email": "fabien.chavonet@example.com"}'
+curl -X PUT http://localhost:5000/api/v1/users/$USER -H "Content-Type: application/json" -H "Authorization: Bearer $JWT" -d '{"first_name": "Fabien", "last_name": "Chavonet", "email": "hugo.chilemme@example.com", "password": "ABCD1234"}'
 
 echo -e "\n> Get New User Details:"
 curl -X GET http://localhost:5000/api/v1/users/$USER
