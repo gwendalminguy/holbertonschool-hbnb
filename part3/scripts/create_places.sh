b2db0a263af9e9d122ba46895183236d2c0bf5d9
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-08 16:44:20 +0200

    Updated bash script

--- part3/scripts/create_places.sh
+++ part3/scripts/create_places.sh
@@ -16,5 +16,4 @@ echo "Access Token: $JWT"
 echo -e "\n> Create New Places:"
 # curl -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT" -d '{"title": "Californian Apartment", "description": "Beautiful place with a view on the beach.", "price": 550.0, "latitude": 65.5, "longitude": -172.5, "owner_id": "'"$OWNER"'", "rooms": 6, "capacity": 8, "surface": 85.5, "amenities": []}'
 # curl -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT" -d '{"title": "Nice French Apartment", "description": "Cozy apartment in the center of Toulouse", "price": 225.0, "latitude": 85, "longitude": 125, "owner_id": "'"$OWNER"'", "rooms": 3, "capacity": 4, "surface": 55.5, "amenities": []}'
-
 curl -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT" -d '{"title": "Californian Apartment", "description": "Beautiful place with a view on the beach.", "price": 550.0, "latitude": 65.5, "longitude": -172.5, "owner_id": "'"$OWNER"'", "rooms": 6, "capacity": 8, "surface": 85.5}'
--- part3/scripts/update_review.sh
+++ part3/scripts/update_review.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 echo -e "\n> Create New User (Place Owner):"
-RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/json" -d '{"first_name": "Johnathan", "last_name": "Doe", "email": "johnathan.doe@example.com", "password": "abcd1234"}')
+RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/json" -d '{
+	"first_name": "Johnathan",
+	"last_name": "Doe",
+	"email": "johnathan.doe@example.com",
+	"password": "abcd1234",
+	"is_admin": 1
+}')
 export OWNER="$(echo "$RESPONSE" | jq -r '.id')"
 echo "Place Owner ID: $OWNER"
 
@@ -14,15 +20,16 @@ export JWT_OWNER="$(echo "$RESPONSE" | jq -r '.access_token')"
 echo "Access Token: $JWT_OWNER"
 
 echo -e "\n> Create New Amenities:"
-RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/amenities/ -H "Content-Type: application/json" -d '{"name": "Parking"}')
+RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/amenities/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT_OWNER" -d '{"name": "Parking"}')
 export AMENITY_1="$(echo "$RESPONSE" | jq -r '.id')"
 echo "Amenity ID: $AMENITY_1"
-RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/amenities/ -H "Content-Type: application/json" -d '{"name": "Wi-Fi"}')
+RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/amenities/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT_OWNER" -d '{"name": "Wi-Fi"}')
 export AMENITY_2="$(echo "$RESPONSE" | jq -r '.id')"
 echo "Amenity ID: $AMENITY_2"
 
 echo -e "\n> Create New Place:"
-RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT_OWNER" -d '{"title": "Nice French Apartment", "description": "Cozy apartment in the center of Toulouse", "price": 225.0, "latitude": 85, "longitude": 125, "owner_id": "'"$OWNER"'", "rooms": 3, "capacity": 4, "surface": 55.5, "amenities": [{"id": "'"$AMENITY_1"'"}, {"id": "'"$AMENITY_2"'"}]}')
+# RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT_OWNER" -d '{"title": "Nice French Apartment", "description": "Cozy apartment in the center of Toulouse", "price": 225.0, "latitude": 85, "longitude": 125, "owner_id": "'"$OWNER"'", "rooms": 3, "capacity": 4, "surface": 55.5, "amenities": [{"id": "'"$AMENITY_1"'"}, {"id": "'"$AMENITY_2"'"}]}')
+RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT_OWNER" -d '{"title": "Nice French Apartment", "description": "Cozy apartment in the center of Toulouse", "price": 225.0, "latitude": 85, "longitude": 125, "owner_id": "'"$OWNER"'", "rooms": 3, "capacity": 4, "surface": 55.5}')
 export PLACE="$(echo "$RESPONSE" | jq -r '.id')"
 echo "Place ID: $PLACE"
 
@@ -40,9 +47,9 @@ export JWT_USER="$(echo "$RESPONSE" | jq -r '.access_token')"
 echo "Access Token: $JWT_USER"
 
 echo -e "\n> Create New Reviews:"
-RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/reviews/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT_USER" -d '{"title": "Great place!", "text": "I had a very good time in Toulouse. Hope to come back soon!", "rating": 4, "user_id": "'"$USER"'", "place_id": "'"$PLACE"'"}')
-export REVIEW_1="$(echo "$RESPONSE" | jq -r '.id')"
-echo "Review ID: $REVIEW_1"
+# RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/reviews/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT_USER" -d '{"title": "Great place!", "text": "I had a very good time in Toulouse. Hope to come back soon!", "rating": 4, "user_id": "'"$USER"'", "place_id": "'"$PLACE"'"}')
+# export REVIEW_1="$(echo "$RESPONSE" | jq -r '.id')"
+# echo "Review ID: $REVIEW_1"
 RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/reviews/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT_USER" -d '{"title": "Hot and noisy...", "text": "Great city, but the apartment lacks air conditioning and the neighbours were pretty loud!", "rating": 2, "user_id": "'"$USER"'", "place_id": "'"$PLACE"'"}')
 export REVIEW_2="$(echo "$RESPONSE" | jq -r '.id')"
 echo "Review ID: $REVIEW_2"
@@ -53,9 +60,9 @@ curl -X GET http://localhost:5000/api/v1/places/$PLACE/reviews
 echo -e "\n> Get Place:"
 curl -X GET http://localhost:5000/api/v1/places/$PLACE
 
-echo -e "\n> Delete Review:"
-curl -X DELETE http://localhost:5000/api/v1/reviews/$REVIEW_1 -H "Authorization: Bearer $JWT_USER"
-echo "Review ID: $REVIEW_1"
+# echo -e "\n> Delete Review:"
+# curl -X DELETE http://localhost:5000/api/v1/reviews/$REVIEW_1 -H "Authorization: Bearer $JWT_USER"
+# echo "Review ID: $REVIEW_1"
 
 echo -e "\n> Put Review:"
 curl -X PUT http://localhost:5000/api/v1/reviews/$REVIEW_2 -H "Content-Type: application/json" -H "Authorization: Bearer $JWT_USER" -d '{"title": "Not that bad...", "text": "Thinking about it, the place itself was not that bad. The surrouding atmosphere was the problem, but this was not a problem coming from the place.", "rating": 3, "user_id": "'"$OWNER"'", "place_id": "'"$PLACE"'"}'
