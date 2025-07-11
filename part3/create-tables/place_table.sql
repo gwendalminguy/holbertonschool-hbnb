38d1377e70659cecb7f09fa4dfeaaee5fcbe5a29
Mornac <imornac@gmail.com>
2025-07-11 09:34:45 +0200

    add not null to user, review and place tables

--- part3/create-tables/place_table.sql
+++ part3/create-tables/place_table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS place (
-    id CHAR(36) PRIMARY KEY (UUID format),
-    title VARCHAR(255),
-    description TEXT,
-    price DECIMAL(10, 2),
-    latitude FLOAT,
-    longitude FLOAT,
-    rooms INTEGER,
-    capacity INTEGER,
-    surface FLOAT,
-    owner_id CHAR(36) Foreign Key referencing User(id)
+    id CHAR(36) PRIMARY KEY (UUID format), NOT NULL,
+    title VARCHAR(255), NOT NULL,
+    description TEXT, NOT NULL,
+    price DECIMAL(10, 2), NOT NULL,
+    latitude FLOAT, NOT NULL,
+    longitude FLOAT, NOT NULL,
+    rooms INTEGER, NOT NULL,
+    capacity INTEGER, NOT NULL,
+    surface FLOAT, NOT NULL,
+    owner_id CHAR(36) Foreign Key referencing User(id), NOT NULL
 );
--- part3/create-tables/review_table.sql
+++ part3/create-tables/review_table.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS Review (
     Rating INTEGER CHECK (Rating >= 1 AND Rating <= 5),
     user_id CHAR(36) NOT NULL, Foreign Key referencing User(id)
     place_id CHAR(36) NOT NULL, Foreign Key referencing Place(id)
-    UNIQUE (user_id, place_id)
+    UNIQUE (user_id, place_id), NOT NULL
 );
