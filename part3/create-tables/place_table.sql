1304c10701cd31e7f0c9243c939de6d2c9bb375b
Mornac <imornac@gmail.com>
2025-07-11 09:54:26 +0200

    fix errors line 1: delete the expression IF NOT EXISTS in the create amenity, place, place_amenity tables

--- part3/create-tables/place_table.sql
+++ part3/create-tables/place_table.sql
@@ -1,12 +1,14 @@
-CREATE TABLE IF NOT EXISTS place (
-    id CHAR(36) PRIMARY KEY (UUID format), NOT NULL,
-    title VARCHAR(255), NOT NULL,
-    description TEXT, NOT NULL,
-    price DECIMAL(10, 2), NOT NULL,
-    latitude FLOAT, NOT NULL,
-    longitude FLOAT, NOT NULL,
-    rooms INTEGER, NOT NULL,
-    capacity INTEGER, NOT NULL,
-    surface FLOAT, NOT NULL,
-    owner_id CHAR(36) Foreign Key referencing User(id), NOT NULL
+CREATE TABLE place (
+    id CHAR(36) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL,
+    rooms INTEGER NOT NULL,
+    capacity INTEGER NOT NULL,
+    surface FLOAT NOT NULL,
+    owner_id CHAR(36) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_id) REFERENCES "user"(id)
 );
