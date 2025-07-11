e47343d1eecbcba4ea362a6251b50554edb87c87
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-11 09:55:51 +0200

    Updated bash script

--- part3/scripts/create_token.sh
+++ part3/scripts/create_token.sh
@@ -2,16 +2,16 @@
 
 echo -e "\n> Create New User:"
 curl -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/json" -d '{
-	"first_name": "Jane",
-	"last_name": "Doe",
-	"email": "jane.doe@example.com",
+	"first_name": "Gwendal",
+	"last_name": "Minguy",
+	"email": "gwendal.minguy@example.com",
 	"password": "abcd1234",
 	"is_admin": 1
 }'
 
 echo -e "\n> Create Token:"
 RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/auth/login -H "Content-Type: application/json" -d '{
-	"email": "jane.doe@example.com",
+	"email": "gwendal.minguy@example.com",
 	"password": "abcd1234"
 }')
 export JWT="$(echo "$RESPONSE" | jq -r '.access_token')"
--- part3/scripts/update_place.sh
+++ part3/scripts/update_place.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# OWNER CREATION
 echo -e "\n> Create New User:"
 RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/users/ -H "Content-Type: application/json" -d '{
 	"first_name": "John",
@@ -19,6 +20,7 @@ RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/auth/login -H "Content-T
 export JWT="$(echo "$RESPONSE" | jq -r '.access_token')"
 echo "Access Token: $JWT"
 
+# AMENITIES CREATION
 echo -e "\n> Create New Amenities:"
 RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/amenities/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT" -d '{"name": "Parking"}')
 export AMENITY_1="$(echo "$RESPONSE" | jq -r '.id')"
@@ -27,18 +29,20 @@ RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/amenities/ -H "Content-T
 export AMENITY_2="$(echo "$RESPONSE" | jq -r '.id')"
 echo "Amenity ID: $AMENITY_2"
 
+# PLACE CREATION
 echo -e "\n> Create New Place:"
-# RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT" -d '{"title": "Californian Apartment", "description": "Beautiful place with a view on the beach.", "price": 550.0, "latitude": 65.5, "longitude": -172.5, "owner_id": "'"$OWNER"'", "rooms": 6, "capacity": 8, "surface": 85.5, "amenities": [{"id": "'"$AMENITY_1"'"}]}')
-RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT" -d '{"title": "Californian Apartment", "description": "Beautiful place with a view on the beach.", "price": 550.0, "latitude": 65.5, "longitude": -172.5, "owner_id": "'"$OWNER"'", "rooms": 6, "capacity": 8, "surface": 85.5}')
+RESPONSE=$(curl -s -X POST http://localhost:5000/api/v1/places/ -H "Content-Type: application/json" -H "Authorization: Bearer $JWT" -d '{"title": "Californian Apartment", "description": "Beautiful place with a view on the beach.", "price": 550.0, "latitude": 65.5, "longitude": -172.5, "owner_id": "'"$OWNER"'", "rooms": 6, "capacity": 8, "surface": 85.5, "amenities": [{"id": "'"$AMENITY_1"'"}]}')
 export PLACE="$(echo "$RESPONSE" | jq -r '.id')"
 echo "Place ID: $PLACE"
 
+# PLACE RETRIEVAL
 echo -e "\n> Get Place Details:"
 curl -X GET http://localhost:5000/api/v1/places/$PLACE
 
+# PLACE UPDATE
 echo -e "\n> Update Place:"
-# curl -X PUT http://localhost:5000/api/v1/places/$PLACE -H "Content-Type: application/json" -H "Authorization: Bearer $JWT" -d '{"title": "Los Angeles Loft", "description": "Beautiful place with a view on the beach.", "price": 850.0, "latitude": 65.5, "longitude": -172.5, "owner_id": "'"$OWNER"'", "rooms": 6, "capacity": 8, "surface": 185.5, "amenities": [{"id": "'"$AMENITY_2"'"}]}'
-curl -X PUT http://localhost:5000/api/v1/places/$PLACE -H "Content-Type: application/json" -H "Authorization: Bearer $JWT" -d '{"title": "Los Angeles Loft", "description": "Beautiful place with a view on the beach.", "price": 850.0, "latitude": 65.5, "longitude": -172.5, "owner_id": "'"$OWNER"'", "rooms": 6, "capacity": 8, "surface": 185.5}'
+curl -X PUT http://localhost:5000/api/v1/places/$PLACE -H "Content-Type: application/json" -H "Authorization: Bearer $JWT" -d '{"title": "Los Angeles Loft", "description": "Beautiful place with a view on the beach.", "price": 850.0, "latitude": 65.5, "longitude": -172.5, "owner_id": "'"$OWNER"'", "rooms": 6, "capacity": 8, "surface": 185.5, "amenities": [{"id": "'"$AMENITY_2"'"}]}'
 
+# PLACE RETRIEVAL
 echo -e "\n> Get New Place Details:"
 curl -X GET http://localhost:5000/api/v1/places/$PLACE
