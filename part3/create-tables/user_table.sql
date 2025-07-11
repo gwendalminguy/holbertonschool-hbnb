8bb4c76318f7d13ed9729072adfc74c470e5a68d
Mornac <imornac@gmail.com>
2025-07-11 09:14:10 +0200

    define SQL script for user table

---
+++ part3/create-tables/user_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS user (
+    id CHAR(36) PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name: VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password: VARCHAR(255),
+    is_admin: BOOLEAN DEFAULT FALSE,
+);
