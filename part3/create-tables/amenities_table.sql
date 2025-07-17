20c08d498854c65e6325148aea92c52b1c4f6092
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-17 18:38:13 +0200

    Removed separated scripts to merge them into one

--- part3/create-tables/amenities_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS amenities (
-    id CHAR(36) PRIMARY KEY NOT NULL,
-    name VARCHAR(255) UNIQUE NOT NULL,
-    created_at DATETIME,
-    updated_at DATETIME
-);
--- part3/create-tables/place_amenity_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS place_amenity (
-    place_id CHAR(36) NOT NULL,
-    amenity_id CHAR(36) NOT NULL,
-    PRIMARY KEY (place_id, amenity_id), 
-    FOREIGN KEY (place_id) REFERENCES places(id),
-    FOREIGN KEY (amenity_id) REFERENCES amenities(id)
-);
