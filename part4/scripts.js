723d2cb122e46ca485574bf38c02057904e9b9c6
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-21 23:29:27 +0200

    Added loginUser

--- part4/scripts.js
+++ part4/scripts.js
@@ -1,8 +1,29 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
+  const loginForm = document.getElementById('login-form');
+
+  if (loginForm) {
+    loginForm.addEventListener('submit', async (event) => {
+      event.preventDefault();
+      loginUser(loginForm.email.value, loginForm.password.value);
+    });
+  }
+});
+
+async function loginUser (email, password) {
+  const response = await fetch('http://127.0.0.1:5000/api/v1/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
   });
+
+  if (response.ok) {
+    const data = await response.json();
+    /*console.log(data);*/
+    document.cookie = `token=${data.access_token}; path=/`;
+    /*window.location.href = 'index.html';*/
+  } else {
+    alert('Login failed: ' + response.statusText);
+  }
+}
