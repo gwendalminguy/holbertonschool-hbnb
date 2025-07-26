cb46b77b1a1493d0e79bfcde4db3bd7b35e83ff9
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-26 19:29:30 +0200

    Updated SQL script

--- part3/scripts/data_insertion.sql
+++ part3/scripts/data_insertion.sql
@@ -16,7 +16,7 @@ VALUES
     ('1fcee8b6-64bc-461d-bb88-0f7697f3a920', 'Garden'),
     ('fd12747f-d73f-4f51-88f6-aff377faec9e', 'Terrace'),
     ('15650aed-dc4a-434e-af46-2771c5a4bf0a', 'Parking'),
-    ('5fd71d38-851d-41c5-a530-34d2e44a2967', 'Washing Machine');
-    ('445bc912-6914-4802-b196-8815d4ded273', 'Coffee Machine');
-    ('ac97ecf8-d04a-4958-a31e-219f1729f3f6', 'Oven');
+    ('5fd71d38-851d-41c5-a530-34d2e44a2967', 'Washing Machine'),
+    ('445bc912-6914-4802-b196-8815d4ded273', 'Coffee Machine'),
+    ('ac97ecf8-d04a-4958-a31e-219f1729f3f6', 'Oven'),
     ('ffa60235-d7e4-40cb-910e-a00fe9cc22d4', 'TV');
