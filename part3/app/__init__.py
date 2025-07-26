a316077570a2a1e1e4cfeb36d2a4d4660276079f
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-26 14:31:19 +0200

    Added OPTIONS method in CORS configuration

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -23,7 +23,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     CORS(app, ressources={
         r"/*": {
             "origins": ["http://localhost:5000", "http://127.0.0.1:5000"],
-            "methods": ["GET", "POST", "PUT", "DELETE"],
+            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
             "allow_headers": ["Content-Type", "Authorization"]
         }
     })
