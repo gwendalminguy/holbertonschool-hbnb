00682fed8979c52d9d77e12e61cc5306affa0f4c
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-30 23:52:41 +0200

    Created bash script for database creation

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p part3/instance
+touch part3/instance/development.db
+
+DATABASE=$(realpath ./part3/instance/development.db)
+SQL=$(which sqlite3)
+CREATION=(./part3/scripts/table_creation.sql)
+INSERTION=(./part3/scripts/data_insertion.sql)
+
+$SQL $DATABASE < $CREATION && $SQL $DATABASE < $INSERTION
