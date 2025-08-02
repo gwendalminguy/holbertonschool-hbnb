9f73504317731df358664f05f1a338f48682a5d3
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-02 11:40:27 +0200

    Added on delete cascade

--- part3/scripts/table_creation.sql
+++ part3/scripts/table_creation.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    id CHAR(36) PRIMARY KEY,
+    id CHAR(36) PRIMARY KEY NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS places (
-    id CHAR(36) NOT NULL,
+    id CHAR(36) PRIMARY KEY NOT NULL,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
@@ -22,8 +22,7 @@ CREATE TABLE IF NOT EXISTS places (
     owner_id CHAR(36) NOT NULL,
     created_at DATETIME,
     updated_at DATETIME,
-    PRIMARY KEY (id),
-    FOREIGN KEY (owner_id) REFERENCES users(id)
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS amenities (
@@ -34,7 +33,7 @@ CREATE TABLE IF NOT EXISTS amenities (
 );
 
 CREATE TABLE IF NOT EXISTS reviews (
-    id CHAR(36) NOT NULL,
+    id CHAR(36) PRIMARY KEY NOT NULL,
     title TEXT NOT NULL,
     text TEXT NOT NULL,
     rating INT NOT NULL,
@@ -42,16 +41,15 @@ CREATE TABLE IF NOT EXISTS reviews (
     place_id CHAR(36) NOT NULL,
     created_at DATETIME,
     updated_at DATETIME,
-    PRIMARY KEY (id),
     UNIQUE (user_id, place_id),
-    Foreign Key (user_id) REFERENCES users(id),
-    Foreign Key (place_id) REFERENCES places(id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (place_id) REFERENCES places(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS place_amenity (
     place_id CHAR(36) NOT NULL,
     amenity_id CHAR(36) NOT NULL,
     PRIMARY KEY (place_id, amenity_id), 
-    FOREIGN KEY (place_id) REFERENCES places(id),
-    FOREIGN KEY (amenity_id) REFERENCES amenities(id)
+    FOREIGN KEY (place_id) REFERENCES places(id) ON DELETE CASCADE,
+    FOREIGN KEY (amenity_id) REFERENCES amenities(id) ON DELETE CASCADE
 );
